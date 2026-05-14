292fce95a8af22c8ea258f710d267f1763210880
andrzguti22 <andrzguti@gmail.com>
2026-05-14 16:00:27 -0500

    feat: agrego archivo de seguimiento de tareas del equipo

---
+++ tareas.py
@@ -0,0 +1,18 @@
+# SEGUIMIENTO DE TAREAS
+
+tareas_pendientes = [
+    'Conexion del frontend con el backend, aplicacion a un trabajo'
+]
+
+tareas_completadas = [
+    'Diseño del frontend'
+]
+
+print('=== TAREAS PENDIENTES ===')
+for t in tareas_pendientes:
+    print(f' ☐{t}') 
+
+print('=== TAREAS COMPLETADAS ===')
+for t in tareas_completadas:
+    print(f' ✓{t}') 
+
